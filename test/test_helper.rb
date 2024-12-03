8f9702c7cf58fd3597d1e6fdae4468f86b77960f
rubyci-setup <rubyci.setup@gmail.com>
2024-12-03 11:21:08 +0100

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,24 +1,4 @@
 # frozen_string_literal: true
-if ENV["FAST_CI_SECRET_KEY"]
-  require "fast_ci"
-  require "rspec/core/runner"
-  require "fast_ci/runner_prepend"
-
-  class RSpec::Core::ExampleGroup
-    def self.filtered_examples
-      ids = Thread.current[:fastci_scoped_ids] || ""
-
-      RSpec.world.filtered_examples[self].filter do |ex|
-        ids == "" || /^#{ids}($|:)/.match?(ex.metadata[:scoped_id])
-      end
-    end
-  end
-
-  RSpec::Core::Runner.prepend(FastCI::RunnerPrepend)
-end
-
-require "simplecov"
-SimpleCov.start "rails"
 
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
